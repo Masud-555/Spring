06ce54ac9c19f74a32e66b432402f464219e2b0d
Masud-555 <abdullahalmasud011@gmail.com>
2025-08-02 19:04:40 +0600

    add

--- project/src/main/java/com/emranhss/project/config/WebConfig.java
+++ project/src/main/java/com/emranhss/project/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.emranhss.project.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
 
 
     @Value("${image.upload.dir}")
