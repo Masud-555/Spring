fbaf05dd6f3c6054d3e558a91c0c7e0f8462ddea
Masud-555 <abdullahalmasud011@gmail.com>
2025-08-20 19:08:14 +0600

    add

--- restuarent/src/main/java/com/emranhss/restuarent/entity/BookTable.java
+++ restuarent/src/main/java/com/emranhss/restuarent/entity/BookTable.java
@@ -1,4 +1,101 @@
 package com.emranhss.restuarent.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class BookTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private Date date;
+    private int time;
+    private String guest;
+    private String massage;
+
+    public BookTable() {
+    }
+
+    public BookTable(Long id, String name, String email, String phone, Date date, int time, String guest, String massage) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.date = date;
+        this.time = time;
+        this.guest = guest;
+        this.massage = massage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getGuest() {
+        return guest;
+    }
+
+    public void setGuest(String guest) {
+        this.guest = guest;
+    }
+
+    public String getMassage() {
+        return massage;
+    }
+
+    public void setMassage(String massage) {
+        this.massage = massage;
+    }
 }
